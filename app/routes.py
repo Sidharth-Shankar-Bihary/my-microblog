b12c8d595199dd222157be00b0c62d51730eec8a
Sidharth-Shankar-Bihary <sidharth.bihary@gmail.com>
2021-08-23 21:04:43 +0530

    jinja2 template

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,9 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Sidharth'}
-    return '''
-<html>
-<head>
-<title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    return render_template('index.html', title='Home', username=user['username'])
