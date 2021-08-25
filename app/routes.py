f0bb258c17fce7051c1a5151d8dc585b9f35318e
Sidharth-Shankar-Bihary <sidharth.bihary@gmail.com>
2021-08-25 17:13:43 +0530

    register form

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,8 @@ from flask_login import logout_user
 from flask import request
 from werkzeug.urls import url_parse
 from flask_login import login_required
+from app import db
+from app.forms import RegistrationForm
 
 
 @app.route('/')
@@ -48,3 +50,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
