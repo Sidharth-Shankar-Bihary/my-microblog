37e1c7576a6abaddbe40c02c42074da146439acc
Sidharth-Shankar-Bihary <sidharth.bihary@gmail.com>
2021-08-26 00:16:59 +0530

    error handling

--- config.py
+++ config.py
@@ -7,3 +7,20 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATION = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get(
+        'MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['microblogger.flask@gmail.com']
+
+
+'''
+export FLASK_DEBUG=1
+export MAIL_SERVER=smtp.googlemail.com 
+export MAIL_PORT=587
+export MAIL_USE_TLS=1
+export MAIL_USERNAME='microblogger.flask@gmail.com'
+export MAIL_PASSWORD='Sidmicroblogger@123'
+'''
