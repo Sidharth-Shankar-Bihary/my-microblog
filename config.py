3041785c6216d410bded1d86ca55547e619b9e49
Sidharth-Shankar-Bihary <sidharth.bihary@gmail.com>
2021-08-29 16:59:24 +0530

    email integration

--- config.py
+++ config.py
@@ -19,7 +19,8 @@ class Config(object):
 
 '''
 export FLASK_DEBUG=1
-export MAIL_SERVER=smtp.googlemail.com 
+
+export MAIL_SERVER=smtp.googlemail.com
 export MAIL_PORT=587
 export MAIL_USE_TLS=1
 export MAIL_USERNAME='microblogger.flask@gmail.com'
