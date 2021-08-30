c5770d00563ec70780211caec9ad0afde2936c9c
Sidharth-Shankar-Bihary <sidharth.bihary@gmail.com>
2021-08-30 20:58:29 +0530

    adding css

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 import os
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 
 app = Flask(__name__)
@@ -17,6 +19,9 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 from app import routes, models, errors
 
