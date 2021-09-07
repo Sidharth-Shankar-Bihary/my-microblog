eb144ae136bd864274682f99468aa895ffb7ed5e
Sidharth-Shankar-Bihary <sidharth.bihary@gmail.com>
2021-09-07 16:36:47 +0530

    user popup

--- app/routes.py
+++ app/routes.py
@@ -228,3 +228,10 @@ def search():
     prev_url = url_for(search, q=g.search_form.q.data,
                        page=page-1) if page > 1 else None
     return render_template('search.html', title='Search', posts=posts, next_url=next_url, prev_url=prev_url)
+
+
+@app.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user_popup.html', user=user)
